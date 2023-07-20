bd1b0bafaf4a12a9725c2ff76563559d55c15669
Wayne Camara <p.wayne2802@gmail.com>
2023-07-19 22:37:47 -0300

    feature: Busca e Adição de posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { db } from './firebase';
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
+import {
+  collection,
+  addDoc,
+  getDocs
+} from 'firebase/firestore';
 import { toast } from "react-toastify";
 import './App.css'
 
@@ -8,6 +12,7 @@ function App() {
 
   const [autor, setAutor] = useState('');
   const [titulo, setTitulo] = useState('');
+  const [posts, setPosts] = useState([])
 
   function handleWarn() {
     toast.warn('Os campos devem ser preenchidos', { autoClose: 3000 });
@@ -38,32 +43,80 @@ function App() {
     }
   }
 
+  async function handlesSearchPosts() {
+    const postsRef = collection(db, 'posts');
+    await getDocs(postsRef)
+
+      .then((snapshot: any) => {
+        //@ts-ignore
+        const lista = []
+
+        snapshot.forEach((doc: any) => {
+          // colocando os elemntos no array. ex: id, titulo e autor
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          });
+        });
+        //@ts-ignore
+        setPosts(lista);
+
+      })
+      .catch((erro) => {
+        console.log("erro ao buscar informações" + erro)
+      })
+  }
+
   return (
     <>
       <div className="app-container">
         <h1>React + Fire base</h1>
         <div className="app-content">
-          <label>
-            Titulo:
-          </label>
-          <textarea
-            placeholder='Digite o titulo'
-            value={titulo}
-            onChange={(e) => setTitulo(e.target.value)}
-          />
+          <div className="field-content">
+            <label>
+              Titulo:
+            </label>
+            <textarea
+              placeholder='Digite o titulo'
+              value={titulo}
+              onChange={(e) => setTitulo(e.target.value)}
+            />
+          </ div>
+          <div className="field-content">
+            <label>
+              Autor:
+            </label>
+            <textarea
+              placeholder='Digite o autor'
+              value={autor}
+              onChange={(e) => setAutor(e.target.value)}
+            />
+          </ div>
 
-          <label>
-            Autor:
-          </label>
-          <textarea
-            placeholder='Digite o autor'
-            value={autor}
-            onChange={(e) => setAutor(e.target.value)}
-          />
+          <div className="btn-container">
+            <button onClick={() => handleAddDocument()}>
+              Cadastrar
+            </button>
+            <button onClick={() => handlesSearchPosts()}>
+              Buscar posts
+            </button>
+          </div>
 
-          <button onClick={() => handleAddDocument()}>
-            Cadastrar
-          </button>
+          <div className="list-container">
+            <ul>
+              {posts.map((post: any) => {
+                return (
+                  <div className="list-content">
+                  <li key={post.id}>
+                    <span>Title: {post.titulo}</span> <br />
+                    <span>Autor: {post.autor} </span> <br />
+                  </li>
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </>
