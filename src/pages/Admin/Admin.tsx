11e21de472f1e672b92a0ea14bbbcffc39eec72a
Wayne Camara <p.wayne2802@gmail.com>
2023-07-27 12:53:01 -0300

    Rotas privadas

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+
+function Admin() {
+  return (
+    <div>AOOOOOOOOOOOOOOOOOOOo</div>
+  )
+}
+
+export default Admin
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,70 @@
+import { useState } from 'react';
 import './style.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { handleSuccess, handleWarn, handleWarnLogin } from '../../components/Utils/Utils';
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 function Home() {
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const navigate = useNavigate();
+
+
+  async function handleLogin(e: any) {
+    e.preventDefault();
+
+    {
+      email !== '' && senha !== ''
+        ?
+        await signInWithEmailAndPassword(auth, email, senha)
+          .then(() => {
+            navigate('/admin', { replace: true })
+            handleSuccess();
+            setSenha('')
+            setEmail('')
+          })
+          .catch((erro: any) => {
+            console.log("Erro em: " + erro)
+            handleWarnLogin()
+            setSenha('')
+
+          })
+        :
+        handleWarn()
+
+    }
+  }
+
   return (
-    <div>Home</div>
+    <section>
+      <div className="home-page-container">
+        <h1>Lista de tarefas</h1>
+        <h2>Organize suas atividades diaria.!</h2>
+
+        <form className='form-task-list' onSubmit={handleLogin}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Digite seu email'
+            type='text' />
+
+          <input
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder='********'
+            type='password' />
+
+          <button>Login</button>
+          <Link to={"/login"}>
+            não possui uma conta? Cadastre-se.
+          </Link>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default Home
+export default Home;
